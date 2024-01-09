e6fdf4a7b0ab7767b609c17a616af6ef7a5ce1d5
joshuasorkin <joshua.d.sorkin@gmail.com>
2024-01-08 16:10:00 -0800

    log rackrect and imagerect

--- public/script.js
+++ public/script.js
@@ -27,6 +27,7 @@ scrambledWord.addEventListener('click', function() {
 
 function handleSubmissionSuccess(result){
     deleteOverlayCanvas();
+    console.log("removing drag tab events...");
     removeDragTabEvents();
     console.log("removing tile drag events...");
     removeRackEventListeners();
@@ -257,6 +258,7 @@ function endTabDrag(evt) {
     // Check if rack overlaps with game image
     //if (rackRect.bottom > imageRect.top && rackRect.top < imageRect.bottom) {
     if (rackRect.top < imageRect.bottom) {
+        console.log("rackRect.top",rackRect.top,"imageRect.bottom",imageRect.bottom);
         console.log("Rack reached the game image!");
         // Handle the event where rack overlaps with game image
         submitGuess();

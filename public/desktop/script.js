ba25d297ad588d291e095c763c3a9b9aa2ff4845
joshuasorkin <joshua.d.sorkin@gmail.com>
2024-03-24 02:11:56 -0700

    add spacing

--- public/desktop/script.js
+++ public/desktop/script.js
@@ -2,6 +2,6 @@
 const response = await fetch(`../api/default-contact`);
 const default_contact_info = await response.text();
 document.getElementById('game-message').innerHTML = "<h1>Desktop Site Under Development</h1>"+
-                                                    "<p>Our desktop site is currently under development."+
+                                                    "<p>Our desktop site is currently under development.  "+
                                                     "Please visit us on a mobile device in the meantime. Thank you for your understanding!</p>"+
                                                     `<p>Contact: <a href="${default_contact_info}" target="_blank">${default_contact_info}</a></p>`

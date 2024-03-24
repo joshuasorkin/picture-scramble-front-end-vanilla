02b33b30f5ce26449d47547954aff1803755401b
joshuasorkin <joshua.d.sorkin@gmail.com>
2024-03-24 01:44:36 -0700

    change api url

--- public/desktop/script.js
+++ public/desktop/script.js
@@ -1,5 +1,5 @@
 
-const response = await fetch(`/api/default-contact`);
+const response = await fetch(`../api/default-contact`);
 const default_contact_info = await response.text();
 document.getElementById('game-message').innerHTML = "<h1>Desktop Site Under Development</h1>"+
                                                     "<p>Our desktop site is currently under development."+
